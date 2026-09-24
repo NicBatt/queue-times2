c42a216cf00dccafffe62cbd4a909363323387d6
NicBatt <Cosmicnic6.02@gmail.com>
2026-09-23 21:48:10 -0500

    Update proxy URL for API requests

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ class ParkWaitTimesApp {
         this.currentParkMeta = this.supportedParks.find(p => p.id === this.currentParkId);
 
         this.apiConfig = {
-            ⁠proxyUrl: '[https://api.allorigins.win/raw?url=](https://api.allorigins.win/raw?url=)'⁠,
+        proxyUrl: 'https://corsproxy.io/?key=1ccd4391&url=',
         };
         
         this.waitTimeData = { lands: [] };
